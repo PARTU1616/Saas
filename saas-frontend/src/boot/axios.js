c9e856aee58bbe0204db80d6fe4d62aedc1372b7
prathameshgalugade-incentius <prathamesh.galugade@incentius.com>
2026-04-30 20:14:08 +0530

    fix: ensure HTTPS in axios baseURL

--- saas-frontend/src/boot/axios.js
+++ saas-frontend/src/boot/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useAuthStore } from 'stores/auth'
 
 const api = axios.create({
-  baseURL: 'http://prathameshgalugade.in',
+  baseURL: 'https://prathameshgalugade.in',
 })
 
 export default boot(({ app }) => {
