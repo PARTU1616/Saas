503b76d7e126ebb42ffe2c8869d1a6913650dd97
prathameshgalugade-incentius <prathamesh.galugade@incentius.com>
2026-04-30 12:43:40 +0530

    fix: update CORS to allow vercel frontend

--- saas-backend/app/__init__.py
+++ saas-backend/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-    cors.init_app(app, resources={r"/*": {"origins": "*"}})
+    cors.init_app(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://saas-frontend-gold.vercel.app", "https://prathameshgalugade.in"]}})
     limiter.init_app(app)
 
     # SAFE BOOTSTRAP
